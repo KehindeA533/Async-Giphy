ab353bde05c8fc864afc27c8d9275c09195b9f86
Kehinde. A <whenkehindewalks@gmail.com>
2023-08-01 16:35:53 -0400

    index.js updated

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
+const fs = require("fs");
+const API_KEY = process.env.API_KEY;
+let randomImage = [];
 
-// Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+async function getIamge(query, limit) {
+  //   const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=${limit}&offset=0&rating=g&lang=en`;
+  for (let i = 0; i < limit - 1; i++) {
+    const endpoint = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    randomImage.push(data.data.url);
+  }
+  console.log(randomImage);
+}
+
+getIamge("cat", 25);
